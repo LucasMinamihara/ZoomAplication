d584ccba9ce52b5d9b5bf29c3546e425ab4f967a
Lucas Hideki Minamihara dos Santos <lucas.hmds@outlook.com>
2022-08-06 08:51:21 -0300

    add Home Interface

---
+++ index.js
@@ -0,0 +1,9 @@
+document.getElementById("campo").addEventListener("click",function() { 
+   
+})
+document.getElementById("terca").addEventListener("click", function() { 
+    
+});
+document.getElementById("sabado").addEventListener("click", function() { 
+    
+})
