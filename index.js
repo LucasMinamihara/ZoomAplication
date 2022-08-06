0156b07c8c46da8e2cedae6a1dae40b511e84855
Lucas Hideki Minamihara dos Santos <lucas.hmds@outlook.com>
2022-08-06 14:35:17 -0300

    add newScreen

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-document.getElementById("campo").addEventListener("click",function() { 
-   
+document.getElementById("campo").addEventListener("click",function(e) { 
+newScreen()
 })
-document.getElementById("terca").addEventListener("click", function() { 
-    
+document.getElementById("terca").addEventListener("click", function(e) { 
+newScreen()
 });
-document.getElementById("sabado").addEventListener("click", function() { 
-    
+document.getElementById("sabado").addEventListener("click", function(e) { 
+    newScreen()
 })
+
+
+function newScreen() { 
+    document.querySelector(".mainContainer").classList.add("nothing")
+    document.querySelector(".calcContainer").classList.add("calculator")
+}
